ce4950b02dbb64e65ddc9163be31b63e934f66a6
Praveen Kumar <praveenshakya441@gmail.com>
2024-04-23 17:23:28 +0530

    add order model

--- backend/controllers/cart.controller.js
+++ backend/controllers/cart.controller.js
@@ -1,4 +1,5 @@
 import Cart from "../models/cart.models.js";
+import Product from "../models/product.models.js";
 import { asyncHandler } from "../utils/asyncHandler.js";
 import { ApiError } from "../utils/ApiError.js";
 import { ApiResponse } from "../utils/ApiResponse.js";
@@ -7,10 +8,14 @@ import { ApiResponse } from "../utils/ApiResponse.js";
 const createCartItem = asyncHandler(async(req, res)=>{
     const {name, quantity, image,originalPrice, price, discount, productId, itemColor} = req.body;
     const cartItem = await Cart.findOne({product:productId, user:req.user._id, itemColor});
-
+    
     if(cartItem){
         throw new ApiError(400, "Cart item already exist")
     }
+    
+    // Find Product from the Product table
+    // const item = await Product.findOne({_id:productId, user:req.user._id})  
+    // console.log("item", item);
 
     const cart = await Cart.create({
         user:req.user._id,
@@ -32,7 +37,7 @@ const createCartItem = asyncHandler(async(req, res)=>{
 });
 
 const getCartItemsOfUser = asyncHandler(async(req, res)=>{
-    const cart = await Cart.find({user:req.user._id}).populate(product);
+    const cart = await Cart.find({user:req.user._id}).populate("product");
 
     res.status(200).json(
         new ApiResponse(200, cart, "All cart items")
@@ -89,5 +94,5 @@ export {
     increaseDecreaseCartItemQuantity,
     deleteCartItem,
     deleteAllCartItem,
-    
+
 };
