72899f8d4eecccc88a59b4dd68954ffa07acc8bb
Praveen Kumar <praveenshakya441@gmail.com>
2024-05-02 12:41:39 +0530

    add register page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,18 +1,25 @@
 import Home from "./pages/Home";
 import Header from "./components/Header";
-import {Routes, Route } from "react-router-dom";
+import {Routes, Route, useLocation  } from "react-router-dom";
 import Footer from "./components/Footer";
+import Login from "./pages/profile/Login";
+import Register from "./pages/profile/Register";
+
 
 function App() {
+  const location = useLocation();
+  const hideHeaderAndFooterPaths = ['/login', '/register'];
+  const hideHeaderFooter = hideHeaderAndFooterPaths.includes(location.pathname);
   return (
     <>
-    <Header/>
+    {!hideHeaderFooter && <Header/>}
     <Routes>
-        
+        <Route path="/login" element={<Login/>}/>
+        <Route path="/register" element={<Register/>}/>
         <Route path="/" element={<Home/>}/>
-
+        
     </Routes>
-    <Footer/>
+    {!hideHeaderFooter && <Footer/>}
     </>
   );
 }
---
+++ frontend/src/pages/profile/Register.jsx
@@ -0,0 +1,74 @@
+import React,{useEffect, useState, useRef} from 'react'
+import { MdFace6 } from "react-icons/md";
+import { MdEmail } from "react-icons/md";
+import { RiLockPasswordFill } from "react-icons/ri";
+import { FaImage } from "react-icons/fa";
+import { FaEye } from "react-icons/fa";
+import { FaEyeSlash } from "react-icons/fa6";
+
+const Register = () => {
+    const fileRef = useRef(null);
+
+    const [user, setUser] = useState({
+        name:"",
+        email:"",
+        password:"",
+        image:""
+    })
+
+    const [buttonDisabled, setButtonDisabled] = useState(false)
+    const [pass, setPass] = useState(false)
+    const handleSubmit = ()=>{
+        console.log(user);
+    }
+
+    const showPassword = ()=>{
+        setPass(!pass)
+    }
+
+    useEffect(()=>{
+        if(user.email.length > 0 && user.password.length > 0 && user.name.length > 0){
+            setButtonDisabled(false)
+        }
+        else{
+            setButtonDisabled(true)
+        }
+    },[user])
+
+  return (
+    <div className='max-w-xs md:max-w-sm  mx-auto mt-24 h-auto shadow-lg bg-slate-100 border border-slate-500 p-10 rounded-lg'>
+        <h1 className='text-center text-3xl font-roboto font-medium'>Create your Account</h1>
+        <form className='flex flex-col mt-8' >
+        <div className="flex items-center border-2 border-slate-400 px-2 p-1 rounded-lg mb-6">
+            <MdFace6 className='text-lg'/>
+            <input className='w-full outline-none font-poppins px-2 py-1 bg-inherit' placeholder='Name' required type="text" value={user.name} onChange={(e)=> setUser({...user, name:e.target.value})} />
+        </div>
+
+        <div className="flex items-center border-2 border-slate-400 px-2 p-1 rounded-lg mb-1">
+            <MdEmail/>
+            <input className='w-full font-poppins outline-none px-2 py-1 bg-inherit' placeholder='Email' required type="text" value={user.email} onChange={(e)=> setUser({...user, email:e.target.value})} />
+        </div>
+        <p className='text-xs text-slate-300 m-0 mb-2'>Please Verify the email</p>
+        <div className="flex items-center border-2 border-slate-400 px-2 p-1 rounded-lg mb-6">
+            <RiLockPasswordFill/>
+            <input className='w-full outline-none font-poppins px-2 py-1 bg-inherit'placeholder='Password' required type={pass ? "text" : "password"} value={user.password} onChange={(e)=> setUser({...user, password:e.target.value})} />
+            <span className='cursor-pointer' onClick={showPassword}>{user.password && !pass ? <FaEye/> : <FaEyeSlash/>}</span>
+        </div>
+
+        <div className="flex items-center border-2 border-slate-400 px-2 p-1 rounded-lg">
+            <FaImage onClick={()=> fileRef.current.click()} />
+            
+            <input type="file" ref={fileRef} hidden accept='image/*' onChange={(e)=> setUser({...user, image:e.target.files[0]})} />
+        </div>
+
+        </form>
+        <div className="flex items-center justify-center">
+        <button onClick={handleSubmit} className='bg-slate-400 px-8 py-1 mt-10 text-white rounded-2xl p-medium-18 font-poppins tracking-wider uppercase hover:bg-inherit hover:text-slate-700 hover:border border-slate-700 transition-all ease-in duration-300 hover:scale-105'>
+            {buttonDisabled ? "Fill Details": "Create Account"}
+        </button>
+        </div>
+    </div>
+  )
+}
+
+export default Register
