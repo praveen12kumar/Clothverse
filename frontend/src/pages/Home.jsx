14d976074409c8c3b00ef72fa76941e279f89251
Praveen Kumar <praveenshakya441@gmail.com>
2024-05-10 18:35:25 +0530

    add store overview section

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,13 +1,40 @@
-import React from 'react'
+import React,{useState} from 'react'
 import MainSlider from '../components/mainSlider/MainSlider'
 import CategoryMenu from '../components/categoryMenu/CategoryMenu'
 
+
+
+const storeOverview = [
+  {name:"Best Seller",sortBy: '{"sold":-1,"_id":-1}' },
+  {name : "Newest",sortBy: '{"createdAt":-1,"_id":-1}' },
+  { name : "Discount" ,sortBy : '{"meta.discount":-1,"_id":-1}' },
+  { name : "Top Rate", sortBy : '{"rating":-1,"_id":-1}' }
+]
+
 const Home = () => {
+  const [isActive, setIsActive] = useState(0);
+
   return (
     <div className='z-10'>
       <MainSlider/>
       <CategoryMenu/>
 
+      <div className="wrapper flex flex-col items-center font-poppins my-4 lg:my-8">
+        <div className="">
+          <h1 className='text-4xl my-8 font-roboto font-extrabold leading-tight text-center'>Store Overview</h1>
+          <div className="flex flex-wrap justify-center gap-4 md:gap-8  my-2 ">
+          {
+            storeOverview.map((cond, index)=>(
+              <h2 className={`text-sm md:text-base lg:text-lg  cursor-pointer hover:underline  hover:underline-offset-4 hover:text-slate-600 
+                transition-colors ease-in duration-1000 ${isActive === index ? "text-slate-700 font-medium underline underline-offset-4" :"text-slate-500"}
+              `} key={index} onClick={()=> setIsActive(index)} >{cond.name}</h2>
+            ))
+          }
+          </div>
+        </div>
+
+
+      </div>
 
     </div>
   )
