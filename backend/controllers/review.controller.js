bcb3b587e38daa7dd9b2e148b449fb883e122fc9
Praveen Kumar <praveenshakya441@gmail.com>
2024-05-18 14:23:10 +0530

    refactor delete review controller

--- backend/controllers/review.controller.js
+++ backend/controllers/review.controller.js
@@ -1,8 +1,9 @@
 import ProductReview from "../models/productReview.models.js";
-import ApiError from "../utils/ApiError.js";
-import asyncHandler from "../utils/asyncHandler.js";
-import ApiResponse from "../utils/ApiResponse.js";
+import {ApiError} from "../utils/ApiError.js";
+import {asyncHandler} from "../utils/asyncHandler.js";
+import {ApiResponse} from "../utils/ApiResponse.js";
 import Product from "../models/product.models.js";
+import User from "../models/user.models.js";
 
 
 const createProductReview = asyncHandler(async(req, res)=>{
@@ -81,38 +82,59 @@ const getProductReview = asyncHandler(async(req, res)=>{
 
 const deleteProductReview = asyncHandler(async(req, res)=>{
 
-    const product =await Product.findById(req.params.id);
+    const {productId, reviewId} = req.body;
+
+    const product =await Product.findById(productId);
     if(!product){
         throw new ApiError(404, "Product not found")
     }
 
-    const productReview = await ProductReview.findOne({product:req.params.id, user:req.user._id});
+    const user = await User.findById(req.user._id);
+    if(!user){
+        throw new ApiError(404, "User not found")
+    };
+
+    const productReview = await ProductReview.findById(reviewId);
     if(!productReview){
         throw new ApiError(404, "Review not found")
     }
 
-    await ProductReview.findByIdAndDelete(productReview._id);
+    const correctUser = productReview.user.toString() === req.user._id.toString();
 
-    const productReviews = await ProductReview.find(req.params.id);
+    if(!correctUser){
+        throw new ApiError(401, "You are not authorized to delete this review")
+    }
 
-    let totalRating = 0;
+    const currentRating = Number(product.ratings);
+    const nosRating = product.numberOfReviews;
+    const rate = productReview.rating;
 
-    productReviews.forEach((review)=>{
-        totalRating += review.rating;
-    });
+    const newRating = ((currentRating * nosRating) - rate) / (nosRating - 1);
+    
+    await ProductReview.findByIdAndDelete(reviewId);
 
-    product.numberOfReviews = productReviews.length;
-    product.ratings = (totalRating / productReviews.length).toFixed(1);
+    //const productReviews = await ProductReview.find({product:productId});
+
+    // let totalRating = 0;
+
+    // productReviews.forEach((review)=>{
+    //     totalRating += review.rating;
+    // });
+
+    // product.numberOfReviews = productReviews.length;
+    //product.ratings = (totalRating / productReviews.length).toFixed(1);
+
+    // console.log("ProductRating", product.ratings);
+
+    product.numberOfReviews = product.numberOfReviews -1;
+    product.ratings = newRating;
 
     await product.save({validateBeforeSave:false});
 
     res.status(200).json(
         new ApiResponse(200, {}, "Review deleted successfully")
     )
-
 })
-
-
 
 
 export {createProductReview, getProductReviews, getProductReview, deleteProductReview};
--- backend/routes/review.routes.js
+++ backend/routes/review.routes.js
@@ -1,12 +1,13 @@
 import express from "express";
-import { createProductReview, getProductReviews, getProductReview, deleteReview } from "../controllers/review.controller.js";
+import { createProductReview, getProductReviews, getProductReview, deleteProductReview } from "../controllers/review.controller.js";
 import { isAuthenticatedUser} from "../middlewares/auth.js";
 
 const router = express.Router();
 
 router.route("/product/review").post(isAuthenticatedUser, createProductReview);
+router.route("/product/review").delete(isAuthenticatedUser, deleteProductReview);
 router.route("/product/reviews/:id").get(getProductReviews);
-router.route("/product/review/:id").get(isAuthenticatedUser, getProductReview).delete(isAuthenticatedUser, deleteReview);
+router.route("/product/review/:id").get(isAuthenticatedUser, getProductReview)
 
 
 export default router;
